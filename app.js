1b35c917a59910faf230219a4695b7d5805ceff1
Alexandre Braga <albraga@email.com>
2018-02-16 20:29:06 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,17 @@
+const EpubPress = require('epub-press-js');
+
+var title = process.argv[2];
+var description = process.argv[3];
+var url = process.argv[4];
+
+const ebook = new EpubPress({
+    title: title,
+    description: description,
+    urls: [url]
+});
+
+ebook.publish().then(
+    () => ebook.download()
+).then().catch((error) => {
+    console.log(`Error: ${error}`);
+});
