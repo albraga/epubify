88d39bf45a46cf2177737bfebfd842e12a2dd965
Alexandre Braga <albraga@email.com>
2018-02-19 19:02:20 -0400

    Fix program flow

--- app.js
+++ app.js
@@ -1,24 +1,24 @@
 const EpubPress = require('epub-press-js');
 
 var title = process.argv[2];
-
-if (title === '-h') {
-    console.log('title description url');
-    return;
-}
-
 var description = process.argv[3];
 var url = process.argv[4];
 
-const ebook = new EpubPress({
-    title: title,
-    description: description,
-    urls: [url]
-});
+if (process.argv.length !== 5) {
+    console.log('title description url');
+    return;
+} else {
 
-ebook.publish().then(
-    () => ebook.download()
-).then().catch((error) => {
-    console.log(`Error: ${error}`);
-});
+    const ebook = new EpubPress({
+        title: title,
+        description: description,
+        urls: [url]
+    });
 
+    ebook.publish().then(
+        () => ebook.download()
+    ).then().catch((error) => {
+        console.log(`Error: ${error}`);
+    });
+
+}
